590d5b6aee7e120a738d9b25e500a412d5a60882
isalazera-spec <isalazera@gmail.com>
2025-12-07 22:39:46 +0000

    Work 07.12 - second part Calculator

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,40 @@
+import art
+
 def add(n1, n2):
     return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
 
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+def calc():
+    print(art.logo)
+    f_number = float(input("What's the first number?: "))
+    keep_calculating = True
+    while keep_calculating:
+        for symbol in operations:
+            print(symbol)
+        operator = input("Pick an operation: ")
+        s_number = float(input("What's the second number?: "))
+        f_result = operations[operator](f_number, s_number)
+        print(f"{f_number} {operator} {s_number} = {f_result}")
+        keep = input(f"Type 'y' to continue calculating with {f_result}, or type 'n' to start a new calculation:").lower()
+        if keep == "y":
+            f_number = f_result
+            print(f_number)
+        else:
+            keep_calculating = False
+            calc()
+calc()
--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,6 @@
 def format_name(f_name, l_name):
+    """Take a first name and last name and return a formatted name.
+     - practicing doc strings"""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
@@ -9,4 +11,12 @@ formatted_name = format_name("AnGeLa", "YU")
 length = len(formatted_name)
 
 
+def outer_function(a, b):
+    def inner_function(c, d):
+        return c + d
 
+    return inner_function(a, b)
+
+
+result = outer_function(5, 10)
+print(result)
