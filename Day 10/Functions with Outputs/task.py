fc1b535958eb1ac34d0070b50ce34ffccb17408f
isalazera-spec <isalazera@gmail.com>
2025-12-07 20:20:42 +0000

    Work 07.12

--- Day 10/Functions with Outputs/task.py
+++ Day 10/Functions with Outputs/task.py
@@ -1,1 +1,12 @@
 def format_name(f_name, l_name):
+    # title() capitalizes the first letter and makes the rest of the string lowercase
+    first_name = f_name.title()
+    last_name = l_name.title()
+    return f"{first_name} {last_name}"
+
+f_name = input('Enter first name: ')
+l_name = input('Enter last name: ')
+
+output = format_name(f_name, l_name)
+print(output)
+
--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -1,7 +1,11 @@
 def format_name(f_name, l_name):
-    formated_f_name = f_name.title()
-    formated_l_name = l_name.title()
-    return f"{formated_f_name} {formated_l_name}"
-
+    if f_name == "" or l_name == "":
+        return "You didn't enter anything"
+    else:
+        formated_f_name = f_name.title()
+        formated_l_name = l_name.title()
+        return f"{formated_f_name} {formated_l_name}"
+# all that is typed after the return will not be executed
 
 print(format_name("AnGEla", "YU"))
+
